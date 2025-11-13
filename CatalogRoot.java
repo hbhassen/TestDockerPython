67dba0867e609bfd15f1992fc5a5a5aaff0dc543
Ben Hassen Hamdi <98821944+hbhassen@users.noreply.github.com>
2025-11-13 09:30:21 +0100

    Create CatalogRoot.java

---
+++ CatalogRoot.java
@@ -0,0 +1,16 @@
+package com.example.obsolescence.model;
+
+import java.util.List;
+
+public class CatalogRoot {
+
+    private List<LanguageDefinition> languages;
+
+    public List<LanguageDefinition> getLanguages() {
+        return languages;
+    }
+
+    public void setLanguages(List<LanguageDefinition> languages) {
+        this.languages = languages;
+    }
+}
